92181af2784454f6caba858f66a97dc2026b4b40
Pochepayka <vovapo2004@gmail.com>
2025-11-17 22:46:36 +0000

    Автоматичесткое обновление документации

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['sumarray_0',['sumArray',['../functions_8cpp.html#a3e8533b20b5f669045085b94cf60a8f8',1,'sumArray(const int arr[]):&#160;functions.cpp'],['../functions_8h.html#a3e8533b20b5f669045085b94cf60a8f8',1,'sumArray(const int arr[]):&#160;functions.cpp']]]
+  ['sumarray_0',['sumarray',['../functions_8cpp.html#a3e8533b20b5f669045085b94cf60a8f8',1,'sumArray(const int arr[]):&#160;functions.cpp'],['../functions_8h.html#a3e8533b20b5f669045085b94cf60a8f8',1,'sumArray(const int arr[]):&#160;functions.cpp']]]
 ];
--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['maxelement_0',['maxElement',['../functions_8cpp.html#a7f42a9850aa91b73d169deabd0a45573',1,'maxElement(const int arr[]):&#160;functions.cpp'],['../functions_8h.html#a7f42a9850aa91b73d169deabd0a45573',1,'maxElement(const int arr[]):&#160;functions.cpp']]],
-  ['minelement_1',['minElement',['../functions_8cpp.html#a019b223a077f19abc427018c4d7d2d80',1,'minElement(const int arr[]):&#160;functions.cpp'],['../functions_8h.html#a019b223a077f19abc427018c4d7d2d80',1,'minElement(const int arr[]):&#160;functions.cpp']]]
+  ['maxelement_0',['maxelement',['../functions_8cpp.html#a7f42a9850aa91b73d169deabd0a45573',1,'maxElement(const int arr[]):&#160;functions.cpp'],['../functions_8h.html#a7f42a9850aa91b73d169deabd0a45573',1,'maxElement(const int arr[]):&#160;functions.cpp']]],
+  ['minelement_1',['minelement',['../functions_8cpp.html#a019b223a077f19abc427018c4d7d2d80',1,'minElement(const int arr[]):&#160;functions.cpp'],['../functions_8h.html#a019b223a077f19abc427018c4d7d2d80',1,'minElement(const int arr[]):&#160;functions.cpp']]]
 ];
